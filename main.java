7daa9d1e88e21effaa9d991c92677da271c8cf32
Cafe latte <83046560+Aso2001169@users.noreply.github.com>
2021-07-26 21:29:21 +0900

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+package final2021;
+
+public class main {
+    public static void main(String args[]){
+        int cnt;
+        info.info();
+        masu.masu();
+        info.infocnt();
+        info.numinput();
+
+    }
+}
