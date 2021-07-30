4b829b37539381bcef0a9db4cc6c074789ec9027
Cafe latte <83046560+Aso2001169@users.noreply.github.com>
2021-07-30 15:09:25 +0900

    Add files via upload

---
+++ title.java
@@ -0,0 +1,8 @@
+public class title {
+    public static void start(){
+        System.out.println("*★*:;;;;;:*★*:;;;;;:*★*:;;;;;:*★*:;;;;;:*★*:;;;;;:*★*:;;;;;:*★*");
+        System.out.println("                            戦艦ゲーム    ");
+        System.out.println("*★*:;;;;;:*★*:;;;;;:*★*:;;;;;:*★*:;;;;;:*★*:;;;;;:*★*:;;;;;:*★*");
+    }
+
+}
